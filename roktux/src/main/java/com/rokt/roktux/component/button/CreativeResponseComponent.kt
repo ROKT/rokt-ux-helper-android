c78eeea4f525fc14845c18265035217ea5f7b728
Lewis Krishnamurti <122851605+lewis-rokt@users.noreply.github.com>
2024-11-15 11:17:28 +1100

    TestTagsAsResourceId

--- roktux/src/main/java/com/rokt/roktux/component/button/CreativeResponseComponent.kt
+++ roktux/src/main/java/com/rokt/roktux/component/button/CreativeResponseComponent.kt
@@ -1,11 +1,15 @@
 package com.rokt.roktux.component.button
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import com.rokt.modelmapper.hmap.get
 import com.rokt.modelmapper.mappers.ExperienceModelMapperImpl.Companion.KEY_IS_POSITIVE
 import com.rokt.modelmapper.uimodel.LayoutSchemaUiModel
+import com.rokt.roktux.BuildConfig
 import com.rokt.roktux.component.ComposableComponent
 import com.rokt.roktux.component.LayoutUiModelFactory
 import com.rokt.roktux.component.ModifierFactory
@@ -17,6 +21,7 @@ internal class CreativeResponseComponent(
     private val modifierFactory: ModifierFactory,
 ) : ComposableComponent<LayoutSchemaUiModel.CreativeResponseUiModel> {
 
+    @OptIn(ExperimentalComposeUiApi::class)
     @Composable
     override fun Render(
         model: LayoutSchemaUiModel.CreativeResponseUiModel,
@@ -33,6 +38,7 @@ internal class CreativeResponseComponent(
                 factory = factory,
                 modifierFactory = modifierFactory,
                 modifier = modifier
+                    .semantics { testTagsAsResourceId = BuildConfig.DEBUG }
                     .testTag(
                         if (responseOptionModel.properties.get<Boolean>(KEY_IS_POSITIVE) ==
                             true
