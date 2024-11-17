57b51a56c0583fc9e74921e1723b527d0e9aca2c
Lewis Krishnamurti <122851605+lewis-rokt@users.noreply.github.com>
2024-11-18 10:19:04 +1100

    NI-336 Add Test Tags for Automated Tests (#6)

--- roktux/src/main/java/com/rokt/roktux/component/button/CreativeResponseComponent.kt
+++ roktux/src/main/java/com/rokt/roktux/component/button/CreativeResponseComponent.kt
@@ -1,8 +1,15 @@
 package com.rokt.roktux.component.button
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
+import com.rokt.modelmapper.hmap.get
+import com.rokt.modelmapper.mappers.ExperienceModelMapperImpl.Companion.KEY_IS_POSITIVE
 import com.rokt.modelmapper.uimodel.LayoutSchemaUiModel
+import com.rokt.roktux.BuildConfig
 import com.rokt.roktux.component.ComposableComponent
 import com.rokt.roktux.component.LayoutUiModelFactory
 import com.rokt.roktux.component.ModifierFactory
@@ -14,6 +21,7 @@ internal class CreativeResponseComponent(
     private val modifierFactory: ModifierFactory,
 ) : ComposableComponent<LayoutSchemaUiModel.CreativeResponseUiModel> {
 
+    @OptIn(ExperimentalComposeUiApi::class)
     @Composable
     override fun Render(
         model: LayoutSchemaUiModel.CreativeResponseUiModel,
@@ -29,7 +37,17 @@ internal class CreativeResponseComponent(
                 model = model,
                 factory = factory,
                 modifierFactory = modifierFactory,
-                modifier = modifier,
+                modifier = modifier
+                    .semantics { testTagsAsResourceId = BuildConfig.DEBUG }
+                    .testTag(
+                        if (responseOptionModel.properties.get<Boolean>(KEY_IS_POSITIVE) ==
+                            true
+                        ) {
+                            POSITIVE_BUTTON_TEST_TAG
+                        } else {
+                            NEGATIVE_BUTTON_TEST_TAG
+                        },
+                    ),
                 offerState = offerState,
                 isDarkModeEnabled = isDarkModeEnabled,
                 breakpointIndex = breakpointIndex,
@@ -44,5 +62,10 @@ internal class CreativeResponseComponent(
                 )
             }
         }
+    }
+
+    companion object {
+        private const val POSITIVE_BUTTON_TEST_TAG = "positive_button"
+        private const val NEGATIVE_BUTTON_TEST_TAG = "negative_button"
     }
 }
