e6560d7a6fb743ab43a80dd3150d610357245311
Sahil Suri <sahil.suri@rokt.com>
2025-02-25 16:00:04 +1100

    NI-641 Fix Overlay BackDrop Close

--- roktux/src/main/java/com/rokt/roktux/component/OverlayComponent.kt
+++ roktux/src/main/java/com/rokt/roktux/component/OverlayComponent.kt
@@ -1,6 +1,7 @@
 package com.rokt.roktux.component
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +11,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerEventPass
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.window.Popup
@@ -51,34 +54,26 @@ internal class OverlayComponent(
             }
         }
 
+        // This box is draw edge to edge on the screen as Popup does not draw edge to edge
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .then(insetsPadding)
                 .then(
-                    modifierFactory
-                        .createModifier(
-                            modifierPropertiesList = model.ownModifiers,
-                            conditionalTransitionModifier = model.conditionalTransitionModifiers,
-                            breakpointIndex = breakpointIndex,
-                            isPressed = isPressed,
-                            isDarkModeEnabled = isDarkModeEnabled,
-                            offerState = offerState,
-                        ),
+                    Modifier.background(
+                        modifierFactory
+                            .createBackground(
+                                modifierProperties = model.ownModifiers,
+                                index = breakpointIndex,
+                                isPressed = isPressed,
+                                isDarkModeEnabled = isDarkModeEnabled,
+                            ) ?: Color.Transparent,
+                    ),
                 )
-                .pointerInput(Unit) {
-                    detectTapGestures {
-                        if (model.allowBackdropToClose) {
-                            isClosedByBackdrop = true
-                            onEventSent(LayoutContract.LayoutEvent.CloseSelected(isDismissed = true))
-                        }
-                    }
-                }
-                .animateContentSize()
                 .then(modifier),
         ) {
             Popup(
-                alignment = BiasAlignment(container.arrangementBias, container.alignmentBias),
+                alignment = Alignment.TopStart,
                 properties = PopupProperties(
                     dismissOnBackPress = true,
                     // This is handled by the clickable modifier on the Box
@@ -93,21 +88,49 @@ internal class OverlayComponent(
                     }
                 },
             ) {
-                factory.CreateComposable(
-                    model = model.child,
-                    modifier = Modifier.pointerInput(Unit) {
-                        // This is to avoid sending the click events to parent which may close the overlay
-                        interceptTap(
-                            pass = PointerEventPass.Main,
-                            shouldConsume = true,
-                        ) {}
-                    },
-                    isPressed = isPressed,
-                    offerState = offerState,
-                    isDarkModeEnabled = isDarkModeEnabled,
-                    breakpointIndex = breakpointIndex,
-                    onEventSent = onEventSent,
-                )
+                // This box is to intercept the click events outside the popup as popup handles the click events outside the popup
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .then(
+                            modifierFactory
+                                .createModifier(
+                                    modifierPropertiesList = model.ownModifiers,
+                                    conditionalTransitionModifier = model.conditionalTransitionModifiers,
+                                    breakpointIndex = breakpointIndex,
+                                    isPressed = isPressed,
+                                    isDarkModeEnabled = isDarkModeEnabled,
+                                    offerState = offerState,
+                                ),
+                        )
+                        .pointerInput(Unit) {
+                            detectTapGestures {
+                                if (model.allowBackdropToClose) {
+                                    isClosedByBackdrop = true
+                                    onEventSent(LayoutContract.LayoutEvent.CloseSelected(isDismissed = true))
+                                }
+                            }
+                        }
+                        .animateContentSize()
+                        .then(modifier),
+                    contentAlignment = BiasAlignment(container.arrangementBias, container.alignmentBias),
+                ) {
+                    factory.CreateComposable(
+                        model = model.child,
+                        modifier = Modifier.pointerInput(Unit) {
+                            // This is to avoid sending the click events to parent which may close the overlay
+                            interceptTap(
+                                pass = PointerEventPass.Main,
+                                shouldConsume = true,
+                            ) {}
+                        },
+                        isPressed = isPressed,
+                        offerState = offerState,
+                        isDarkModeEnabled = isDarkModeEnabled,
+                        breakpointIndex = breakpointIndex,
+                        onEventSent = onEventSent,
+                    )
+                }
             }
         }
     }
