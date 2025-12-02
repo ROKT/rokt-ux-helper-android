b05ec07793c098a69fb22e93091ffc85fee1d749
Thomson Thomas <thomson.thomas@rokt.com>
2025-12-02 15:29:05 -0500

    refactor: Consolidate core, modelmapper, and testutils into roktux module
    Merge the modules `core`, `modelmapper`, and `testutils` into the `roktux` module.

--- modelmapper/consumer-rules.pro
+++
@@ -1,1 +0,0 @@
--keep class com.rokt.modelmapper.model.** { *; }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,9 +25,6 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "UxHelper"
-include(":modelmapper")
-include(":core")
 include(":networkhelper")
 include(":roktux")
-include(":testutils")
 include(":demoapp")
