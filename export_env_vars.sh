462534820df9a1ea903b4abb11b864dc560891ba
Thomson Thomas <125323226+thomson-t@users.noreply.github.com>
2025-12-17 17:53:38 -0500

    chore: Remove unused files (#194)
    Deletes the `export_env_vars.sh` script and the `.dockerignore` file.

--- export_env_vars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-VERSION_SUFFIX=$(buildkite-agent meta-data get version_suffix || echo "")
-export VERSION_SUFFIX
-echo "VERSION_SUFFIX=${VERSION_SUFFIX}"
-export ORG_GRADLE_PROJECT_signingInMemoryKey=${SIGNING_KEY}
-export ORG_GRADLE_PROJECT_signingInMemoryKeyPassword=${SIGNING_PASSWORD}
-export ORG_GRADLE_PROJECT_mavenCentralUsername=${SONATYPE_USERNAME}
-export ORG_GRADLE_PROJECT_mavenCentralPassword=${SONATYPE_PASSWORD}
